75b90c358e5a55a0644b2e1c0a7554409658c141
Mads Mætzke Tandrup <mads.tandrup@schneider-electric.com>
2013-06-20 22:00:57 +0200

    Adding basic iPhoto API

---
+++ iPhotoFlickrSync/IPFSLibrary.h
@@ -0,0 +1,15 @@
+//
+//  IPFSLibrary.h
+//  iPhotoFlickrSync
+//
+//  Created by Mads Mætzke Tandrup on 20/06/13.
+//  Copyright (c) 2013 Mads Mætzke Tandrup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPFSLibrary : NSObject
+
+- (void) listAlbums;
+
+@end
---
+++ iPhotoFlickrSync/IPFSLibrary.m
@@ -0,0 +1,37 @@
+//
+//  IPFSLibrary.m
+//  iPhotoFlickrSync
+//
+//  Created by Mads Mætzke Tandrup on 20/06/13.
+//  Copyright (c) 2013 Mads Mætzke Tandrup. All rights reserved.
+//
+
+#import "IPFSLibrary.h"
+#import "iPhoto.h"
+
+@implementation IPFSLibrary
+
+- (void) listAlbums {
+    iPhotoApplication *iPhoto = [SBApplication applicationWithBundleIdentifier:@"com.apple.iPhoto"];
+    
+    if ([iPhoto isRunning]) {
+        NSLog(@"Is running");
+        
+        SBElementArray *albums = [iPhoto albums];
+        
+        if ([albums count] > 0) {
+            for (iPhotoAlbum *album in albums) {
+                NSLog(@"%@", [album name]);
+                SBElementArray *photos = [album photos];
+                
+                NSLog(@"%li", [photos count]);
+                for (iPhotoPhoto *photo in photos) {
+                    NSLog(@"Photo: %@", [photo imagePath]);
+                }
+            }
+        }
+    }
+
+}
+
+@end
